9ec19359044471f91ab9088779ce2cf954edb643
baduyne <125996827+baduyne@users.noreply.github.com>
2023-02-22 00:32:02 +0700

    Add files via upload
    cread card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Đá';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday 22_2';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Happy Birthday';
+const giftImage = 'https://scontent.fsgn2-8.fna.fbcdn.net/v/t1.15752-9/331748793_727822882144323_476729116245172664_n.jpg?stp=dst-jpg_s206x206&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_ohc=us2hV6oyGMkAX9NSrVm&_nc_ht=scontent.fsgn2-8.fna&oh=03_AdQqJ-3En36AHEm3IYtWpB8kAgH1x-CyKiHfJonRyyBtnw&oe=641C6A3D';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
